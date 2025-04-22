5d81cde2cd6afdb75d958166d5863a7d8003859e
Henok-Al <henok.gebresenbet@gmail.com>
2025-04-22 16:45:53 +0300

    connecting to mongodb

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { default: connectDB } = require("./database/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+cors({
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowededHeaders: ["Content-type", "Authorization"],
+});
+
+app.use(express.json());
+
+//database connection
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

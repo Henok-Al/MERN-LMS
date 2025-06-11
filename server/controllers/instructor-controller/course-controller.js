edeb4ebc9bf517335ca5ac9574a7e774aa6f6a49
Henok-Al <henok.gebresenbet@gmail.com>
2025-06-11 15:59:57 +0300

    get all courses

--- server/controllers/instructor-controller/course-controller.js
+++ server/controllers/instructor-controller/course-controller.js
@@ -16,16 +16,26 @@ export const addNewCourse = async (req, res) => {
   } catch (e) {
     console.log(e);
     res.status(500).json({
-        success: false,
-        message: "Some error occured!"
-    })
+      success: false,
+      message: "Some error occured!",
+    });
   }
 };
 
 export const getAllCourses = async (req, res) => {
   try {
+    const courseList = await Course.find({});
+
+    res.status(200).json({
+      success: true,
+      data: courseList,
+    });
   } catch (e) {
     console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
   }
 };
 
